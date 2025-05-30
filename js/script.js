a2f7c869a0ffd2d56dcc1a9085f29e53058a0969
EmanuLuz <emanueli.bessa@escola.pr.gov.br>
2025-05-30 09:17:56 -0300

    criei os arquivos

---
+++ js/script.js
@@ -0,0 +1,11 @@
+alert ("Devolução de Livros");
+
+var nome = prompt ("Digite seu nome: ");
+var email = prompt ("Digite seu Email: ");
+var idade = prompt ("Digite sua Idade: ");
+
+console.log(nome);
+console.log(email);
+console.log(idade);
+
+alert ("Seu cadastro foi concluído com sucesso. ");
